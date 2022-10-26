ae3d122aeb66a9714de593cb916ae36dec88ac9e
Sergey Makletsov <smaklets@kpfu.ru>
2022-10-26 17:53:51 +0300

    удаление комментария

--- src/main/kotlin/ru/smak/Hello.kt
+++ src/main/kotlin/ru/smak/Hello.kt
@@ -4,11 +4,11 @@ import ru.smak.graphics.Converter
 import ru.smak.graphics.Plane
 
 fun main(args: Array<String>) {
-    /*val plane = Plane(
+    val plane = Plane(
         -5.0, 5.0,
         -5.0, 5.0,
         1000, 1000)
-    println(Converter.xScrToCrt(99, plane))*/
+    println(Converter.xScrToCrt(99, plane))
     println("WORKS!")
 }
 
