3df1750ffe6910930c3249666b5473b788f458aa
Sergey Makletsov <smaklets@kpfu.ru>
2022-10-26 17:47:30 +0300

    Initial commit

---
+++ src/main/kotlin/ru/smak/Hello.kt
@@ -0,0 +1,14 @@
+package ru.smak
+
+import ru.smak.graphics.Converter
+import ru.smak.graphics.Plane
+
+fun main(args: Array<String>) {
+    /*val plane = Plane(
+        -5.0, 5.0,
+        -5.0, 5.0,
+        1000, 1000)
+    println(Converter.xScrToCrt(99, plane))*/
+    println("WORKS!")
+}
+
